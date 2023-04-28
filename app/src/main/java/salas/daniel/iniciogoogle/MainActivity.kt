55edd0cccefea21881ab4363c901702705b731b8
DanielSalas11 <daniel_salas11@outlook.com>
2023-04-28 16:58:37 -0700

    Inicio google

--- app/src/main/java/salas/daniel/iniciogoogle/MainActivity.kt
+++ app/src/main/java/salas/daniel/iniciogoogle/MainActivity.kt
@@ -1,11 +1,50 @@
 package salas.daniel.iniciogoogle
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import salas.daniel.iniciogoogle
 
 class MainActivity : AppCompatActivity() {
+    lateinit var gso: GoogleSignInOptions
+    lateinit var gsc: GoogleSignInClient
+    val RC_SIGN_IN = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        gsc = GoogleSignIn.getClient(this, gso);
+
+
+        googleBtn.setOnClickListener{
+            val signInIntent = gsc.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
     }
+
+    private fun signIn() {
+        val signInIntent: Intent = gsc.getSignInIntent()
+        startActivityForResult(signInIntent, 1000)
+    }
+
 }
